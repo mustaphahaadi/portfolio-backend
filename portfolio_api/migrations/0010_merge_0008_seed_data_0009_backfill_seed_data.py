805e57acff035ef54084a9418d256151a49a0747
Mustapha Haadi <mustaphahaadi04@gmail.com>
2026-03-16 17:26:33 +0000

    Add merge migration for seed data branch conflict

---
+++ portfolio_api/migrations/0010_merge_0008_seed_data_0009_backfill_seed_data.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('portfolio_api', '0008_seed_data'),
+        ('portfolio_api', '0009_backfill_seed_data'),
+    ]
+
+    operations = []
